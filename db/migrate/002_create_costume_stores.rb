d3a71937ba144b7a4c40af29d32713c279abffee
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-28 16:26:03 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -3,8 +3,8 @@
 def change 
     create_table :costume_stores do |t|
       t.string :name
-      t.float :custume_inventory
-      t.string :employees_count
+      t.integer :custume_inventory
+      t.integer :employees_count
       t.string :size
       t.timestamps
     end
