ec5a080f832f4c64c6a4461509e905a7fe81dc13
olusola olagoroye <solaolagoroye@gmail.com>
2020-01-28 16:28:38 +0000

    Automatically backed up by Learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,13 +1,12 @@
 # Create your costume_stores migration here
-
-def change 
+ def change
     create_table :costume_stores do |t|
-      t.string:name
+      t.string :name
       t.string :location
-      t.integer :custume_inventory
-      t.integer :employees_count
-      t.string :size
-      t.timestamps
+      t.integer :costume_inventory
+      t.integer :num_of_employees
+      t.boolean :still_in_business
+      t.datetime :opening_time
+      t.datetime :closing_time
     end
   end
-end
